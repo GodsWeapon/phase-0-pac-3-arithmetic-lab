45f5a660b8e4281f236fe7a18c68a06cdf31a8ad
Trafael Lewis <TrafLewis@gmail.com>
2022-03-15 16:00:50 -0700

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+    console.log(a+b)
+}
+
+function subtract() {
+    console.log(2-1)
+}
+
+function multiply() {
+    console.log(1*4)
+}
+
+function divide() {
+    console.log(10/5)
+}
+
+function add(a, b) {
+    return (a + b)
+}
+
+function subtract(a, b) {
+    return (a-b)
+}
+
+function multiply(a, b) {
+    return (a*b)
+}
+
+function divide(a, b) {
+    return (a/b)
+}
+
+function increment(n) {
+    return ++ n
+}
+
+function decrement(n) {
+    return -- n
+}
+
+function makeInt(n) {
+    return parseInt (n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat (n)
+}
